52920a3072f4bf3797c052b5cedb34ce062072be
maziyar parsi <mparsi1992@gmail.com>
2021-07-10 13:02:04 +0430

    add-startGame-function

---
+++ script.js
@@ -0,0 +1,19 @@
+const GAME = () => {
+  let firstPlayerScore = 0;
+  let secondPlayerScore = 0;
+  /////// this is when we click on let's play button///////
+  const startGame = () => {
+    const playButton = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playButton.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+  ////////// end of starting the game process///////////////
+
+  startGame();
+};
+GAME();
