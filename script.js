e8b606e445a00b1edfec02461747f63e0bc33f7c
maziyar parsi <mparsi1992@gmail.com>
2021-07-11 12:55:19 +0430

    add-players-click-functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+let firstPlayerScore = 0;
+let secondPlayerScore = 0;
+
 const GAME = () => {
-  let firstPlayerScore = 0;
-  let secondPlayerScore = 0;
   /////// this is when we click on let's play button///////
   const startGame = () => {
     const playButton = document.querySelector(".intro button");
@@ -14,8 +15,43 @@ const GAME = () => {
   };
   ////////// end of starting the game process///////////////
 
-  ////////// start of real game function ////////////
-
   startGame();
 };
+
 GAME();
+
+////////// start of real game function ////////////
+
+//////////////// functions when players click on start ////////////
+let firstPlayerStart = () => {
+  let handState = Math.floor(Math.random() * 3);
+  if (handState < 1) {
+    handState = "rock";
+    document.getElementById("firstPlayerImage").src = "./images/rock.png";
+  }
+  if (handState < 2) {
+    handState = "scissors";
+    document.getElementById("firstPlayerImage").src = "./images/scissors.png";
+  }
+  if (handState < 3) {
+    handState = "paper";
+    document.getElementById("firstPlayerImage").src = "./images/paper.png";
+  }
+};
+let secondPlayerStart = () => {
+  let handState = Math.floor(Math.random() * 3);
+  if (handState < 1) {
+    handState = "rock";
+    document.getElementById("secondPlayerImage").src = "./images/rock.png";
+  }
+  if (handState < 2) {
+    handState = "scissors";
+    document.getElementById("secondPlayerImage").src = "./images/scissors.png";
+  }
+  if (handState < 3) {
+    handState = "paper";
+    document.getElementById("secondPlayerImage").src = "./images/paper.png";
+  }
+  console.log(handState);
+};
+//////////////// end of functions when players click on start ////////////
